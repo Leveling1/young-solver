e53e497fd6b747a30b0d9556d5d666daf40fe821
Merdi-dev12 <merdipumba@gmail.com>
2026-04-18 13:32:55 +0300

    fix: setup static export, add build cleanup script and update gitignore

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from 'next'
 import { SITE_URL } from '@/content/site-config'
 
+export const dynamic = 'force-static'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
